c92170baddaae7b2b94dff8252aee1a6b0d204e0
Xiaoyu Chen <xc2525@XiaoyudeMacBook-Pro.local>
2020-11-03 21:46:44 +0800

    debugged MSE and dcg

--- metrics_helper.py
+++ metrics_helper.py
@@ -7,16 +7,16 @@ def MAE(test, pred):
 
 
 def MSE(test, pred):
-    n = test.shape[0]*test.shape[1] - np.isnan(pred)
-    return np.nansum((test-pred)**2)/n
+    return np.nanmean((test-pred)**2)
 
 
 def dcg(test, pred):
     m = test.shape[1]
     sorted_pred = -np.sort(-pred, axis=0)
     sorted_indice = np.argsort(-pred, axis=0)
-    sorted_test = test[sorted_indice]
+    sorted_test = np.zeros(test.shape)
     ranking = np.zeros(test.shape)
     for i in range(m):
-        ranking[:][i] = i + 1
+        sorted_test[:, i] = test[:, i][sorted_indice[:, i]]
+        ranking[:, i] = i + 1
     return np.mean(np.nansum((np.where(np.isnan(sorted_test), np.nan, 1))/np.log2(ranking+1), axis=1))
